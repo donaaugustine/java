5920872d223d7ac8acfcc15f85bce59ab1d3ab83
donaaugustine <74652290+donaaugustine@users.noreply.github.com>
2021-08-04 23:56:15 +0530

    Add files via upload

--- Cycle2/Employee.java
+++ Cycle2/Employee.java
@@ -1,54 +1,46 @@
-import java.util.Scanner;
-public class Employee
+/*4. Program to create a class for Employee having attributes eNo, eName, eSalary. 
+Read n employ information and Search for an employee given eNo, using the concept of Array of Objects.*/
+
+import java.util.*;
+
+class Employee 
 {
-  public static void main(String[] args) 
-  {
-  int count,flag = 0, i;
-  Scanner scan = new Scanner(System.in);
-  System.out.print("How many  employees information you want enter :");
-  count = scan.nextInt();
-  String eName[] = new String[count];
-  int eSalary[] = new int[count];
-  int  eNo[] = new int[count];
-  Scanner scan2 = new Scanner(System.in);
-  Scanner scan3 = new Scanner(System.in);
-  Scanner scan4 = new Scanner(System.in);
-        
-  for( i = 0; i < count; i++)
-  {
-      System.out.println("Enter the eNo of Employee "+(i+1)+ ":");
-      eNo[i] = scan2.nextInt();
-      System.out.println("Enter the name :");
-      eName[i] = scan3.nextLine();
-      System.out.println("Enter the salary :");
-      eSalary[i] = scan4.nextInt();
-  }
-  Scanner s = new Scanner(System.in);
-  System.out.print("Enter the employee number you want to find:");
-  int x = s.nextInt();
-  for( i = 0; i < count; i++)
-  {
-     if(eNo[i] == x)
-     {
-          flag = 1;
-          break;
-      }
-  }
-  if(flag == 1)
-  {
-     int t = i;
-     System.out.println("Employee id "+x+" found");
-     System.out.println("Employee name : "+eName[t]);
-     System.out.println("Employee salary : "+eSalary[t]); 
-  }
-  else
-  {
-      System.out.println("Employee id not found");
-  }
-  scan.close();
-  scan2.close();
-  scan3.close();
-  scan4.close();
-  s.close();
-  }
+int eNo;
+String eName;
+float eSalary;
+ 
+public void getdata() 
+{
+Scanner s = new Scanner(System.in);
+System.out.println("enter the employee number : ");
+eNo=s.nextInt();
+System.out.println("enter the employee name : ");
+eName=s.next();
+System.out.println("enter the employee salary : ");
+eSalary=s.nextFloat();
+} 
+public void display() 
+{
+System.out.println("\nemployee number : " +eNo);
+System.out.println("employee name : " +eName);
+System.out.println("employee salary : " +eSalary);
+}
+public static void main(String args[]) 
+{
+int n=0;
+Scanner s1 = new Scanner(System.in);
+System.out.println("enter the limit :");
+n=s1.nextInt();
+Employee e[]=new Employee[n];
+for(int i=0; i<n; i++) 
+{
+e[i]=new Employee();
+e[i].getdata();
+}
+System.out.println("\n***EMPLOYEE DETAILS***");
+for(int i=0; i<n; i++) 
+{
+e[i].display();
+}
+}
 }
